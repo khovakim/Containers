de25b8a921e230bf72b722f4c2dc9b479b0124ec
Khachik Hovakimyan <khovakim@c1r1s6.42yerevan.am>
2023-12-04 21:16:56 +0400

    update

--- Vector/vector.h
+++ Vector/vector.h
@@ -28,8 +28,23 @@ namespace cp
 			using const_iterator  = cp::random_access_iterator<const value_type>;
 
 		public:
-			constexpr vector();
-			explicit  vector(size_type count, const reference value = T());
+			constexpr          vector() noexcept;
+			explicit constexpr vector(size_type count);
+			constexpr          vector(size_type count, const reference value);
+			explicit           vector(size_type count, const reference value = T());
+			constexpr          vector(const vector& other);
+			constexpr          vector(vector&& other)                noexcept;
+			constexpr          vector(std::initializer_list<T> init) noexcept;
+
+			~vector();
+
+			constexpr vector& operator=(const vector& other);
+			constexpr vector& operator=(vector&& other) noexcept;
+			constexpr vector& operator=(std::initializer_list<T> init);
+
+		public:
+			
+
 		private:
 			size_type msize;
 			size_type mcapacity;
